f7166e7ff2750ebe81410e7cb56a7ea37d735e56
Данил Шифман <danilshifman07@gmail.com>
2024-12-05 21:36:45 +0300

    Sprint 9

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'acme_project.wsgi.application'
 
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -79,7 +83,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from django.views.generic import (
+ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+
+from .forms import BirthdayForm
+from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=instance
+    )
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request, 'birthday/birthday.html', context)
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = ('id',)
+    paginate_by = 10
