63ad0908da1e06cceb0b95820e23b2b9ebe8d33b
Данил Шифман <danilshifman07@gmail.com>
2024-12-07 18:36:03 +0300

    New SpawnPoint

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -21,7 +21,8 @@ INSTALLED_APPS = [
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
     'django_bootstrap5',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -32,6 +33,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'acme_project.urls'
--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -18,6 +18,12 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 handler404 = 'core.views.page_not_found'
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
